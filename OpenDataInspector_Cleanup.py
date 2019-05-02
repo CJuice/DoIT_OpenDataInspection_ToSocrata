ba2fcec2cfcefdcea4432a08ff5cbc58302fdb81
CJuice <CJuice@users.noreply.github.com>
2019-05-02 15:00:56 -0400

    Update OpenDataInspector_Cleanup.py

--- OpenDataInspector_Cleanup.py
+++ OpenDataInspector_Cleanup.py
@@ -126,14 +126,14 @@ def main():
     socrata_overview_level_dataset_app_id = get_dataset_identifier(credentials_json=credentials_json,
                                                                    dataset_key="overview_level_dataset")
 
-
-
     # Overview level operations
+    print("Entering Overview Dataset Operations")
     more_overview_records_exist_than_response_limit_allows = True
-    overview_cycle_record_count = 0
     overview_total_record_count = 0
     overview_record_offset_value = 0
+
     while more_overview_records_exist_than_response_limit_allows:
+        overview_cycle_record_count = 0
         overview_dataset_url = build_dataset_url(url_root=ROOT_URL_FOR_DATASET_ACCESS,
                                                  api_id=socrata_overview_level_dataset_app_id,
                                                  limit_amount=LIMIT_MAX_AND_OFFSET,
@@ -149,15 +149,12 @@ def main():
             overview_row_id = obj.get("row_id", None)
             if overview_date_obj < baseline_date:
                 overview_outdated_row_ids_list.append({":row_id": overview_row_id, ":deleted": True})
-        print(overview_outdated_row_ids_list)
 
         number_of_overview_records_returned = len(overview_json)
-        print(number_of_overview_records_returned)
         overview_cycle_record_count += number_of_overview_records_returned
         overview_total_record_count += number_of_overview_records_returned
 
         # Any cycle_record_count that equals the max limit indicates another request is needed
-        print(overview_cycle_record_count)
         if overview_cycle_record_count == LIMIT_MAX_AND_OFFSET:
             # Give Socrata servers small interval before requesting more
             time.sleep(0.2)
@@ -165,23 +162,27 @@ def main():
         else:
             more_overview_records_exist_than_response_limit_allows = False
 
+    print(overview_outdated_row_ids_list)
+
     # socrata_client_overview_level.upsert(dataset_identifier=socrata_overview_level_dataset_app_id,
     #                                      payload=overview_outdated_row_ids_list,
     #                                      content_type='json')
     socrata_client_overview_level.close()
-    exit()
+
 
     # Field level operations
+    print("Entering Field Dataset Operations")
     more_field_records_exist_than_response_limit_allows = True
-    field_cycle_record_count = 0
     field_total_record_count = 0
     field_record_offset_value = 0
     while more_field_records_exist_than_response_limit_allows:
+        field_cycle_record_count = 0
         field_dataset_url = build_dataset_url(url_root=ROOT_URL_FOR_DATASET_ACCESS,
                                               api_id=socrata_field_level_dataset_app_id,
                                               limit_amount=LIMIT_MAX_AND_OFFSET,
                                               offset=field_record_offset_value,
                                               total_count=field_total_record_count)
+        print(field_dataset_url)
         field_response = requests.get(url=field_dataset_url)
         field_json = field_response.json()
 
@@ -190,13 +191,21 @@ def main():
             field_date_obj = parser.parse(field_date)
             field_row_id = obj.get("row_id", None)
             if field_date_obj < baseline_date:
-                overview_outdated_row_ids_list.append({":row_id": field_row_id, ":deleted": True})
+                field_outdated_row_ids_list.append({":row_id": field_row_id, ":deleted": True})
+
         number_of_field_records_returned = len(field_json)
+        field_cycle_record_count += number_of_field_records_returned
+        field_total_record_count += number_of_field_records_returned
 
+        # Any cycle_record_count that equals the max limit indicates another request is needed
+        if field_cycle_record_count == LIMIT_MAX_AND_OFFSET:
+            # Give Socrata servers small interval before requesting more
+            time.sleep(0.2)
+            field_record_offset_value = field_cycle_record_count + field_record_offset_value
+        else:
+            more_field_records_exist_than_response_limit_allows = False
 
-        #     if field_date_obj < baseline_date:
-        #         field_outdated_row_ids_list.append({":row_id": field_row_id, ":deleted": True})
-        # print(field_outdated_row_ids_list)
+    print(field_outdated_row_ids_list)
 
     # socrata_client_overview_level.upsert(dataset_identifier=socrata_overview_level_dataset_app_id,
     #                                      payload=field_outdated_row_ids_list,
