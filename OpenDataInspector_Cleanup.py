94af430acb5dcaf799d6e753ad3a1a285016dea6
CJuice <CJuice@users.noreply.github.com>
2019-05-08 15:37:39 -0400

    First successful deletion run

--- OpenDataInspector_Cleanup.py
+++ OpenDataInspector_Cleanup.py
@@ -8,13 +8,13 @@
 
 
 def main():
+
     # IMPORTS
     from datetime import datetime
     from sodapy import Socrata
     import dateutil.parser as parser
     import json
     import os
-    import requests
     import time
 
     # VARIABLES
@@ -23,8 +23,12 @@ def main():
     LIMIT_MAX_AND_OFFSET = 10000
     ROOT_MD_OPENDATA_DOMAIN = r"https://opendata.maryland.gov"
     ROOT_URL_FOR_DATASET_ACCESS = r"{root}/resource/".format(root=ROOT_MD_OPENDATA_DOMAIN)
-    SOCRATA_CREDENTIALS_JSON_FILE = os.path.join(_ROOT_URL_FOR_PROJECT,
-                                                 r"EssentialExtraFilesForOpenDataInspectorSuccess\Credentials_OpenDataInspector_ToSocrata.json")
+    SOCRATA_CREDENTIALS_JSON_FILE = os.path.join(
+        _ROOT_URL_FOR_PROJECT,
+        r"EssentialExtraFilesForOpenDataInspectorSuccess\Credentials_OpenDataInspector_ToSocrata_TESTING.json")  # TESTING
+    SOCRATA_CREDENTIALS_JSON_FILE = os.path.join(
+        _ROOT_URL_FOR_PROJECT,
+        r"EssentialExtraFilesForOpenDataInspectorSuccess\Credentials_OpenDataInspector_ToSocrata.json")  # PRODUCTION
     field_outdated_row_ids_list = []
     overview_outdated_row_ids_list = []
 
@@ -44,7 +48,7 @@ def main():
         :return: String url
         """
         # if the record count exceeds the initial limit then the url must include offset parameter
-        if total_count is None and limit_amount == 0 and offset == 0:
+        if total_count == None and limit_amount == 0 and offset == 0:
             return "{}{}".format(url_root, api_id)
         elif total_count >= LIMIT_MAX_AND_OFFSET:
             return "{}{}.json?$limit={}&$offset={}".format(url_root, api_id, limit_amount, offset)
@@ -55,6 +59,8 @@ def main():
         """
         Create and return a Socrata client for use.
 
+        NOTE_1: It seems absolutely essential the the domain be a domain and not a url. 'https://opendata.maryland.gov'
+        will not substitute for 'opendata.maryland.gov'.
         :param credentials_json: the json code from the credentials file
         :param dataset_key: the dictionary key of interest
         :return: Socrata connection client
@@ -64,11 +70,11 @@ def main():
         for key, value in dataset_credentials.items():  # Value of None in json is seen as string, need to convert or fails
             if value == 'None':
                 dataset_credentials[key] = None
+        maryland_domain = dataset_credentials["maryland_domain"]    # SEE NOTE_1 IN FUNCTION DOCUMENTATION
         maryland_app_token = dataset_credentials["app_token"]
         username = access_credentials["username"]
         password = access_credentials["password"]
-        return Socrata(domain=ROOT_MD_OPENDATA_DOMAIN, app_token=maryland_app_token, username=username,
-                       password=password)
+        return Socrata(domain=maryland_domain, app_token=maryland_app_token, username=username, password=password)
 
     def get_dataset_identifier(credentials_json, dataset_key):
         """
@@ -100,21 +106,6 @@ def main():
             filecontents = file_handler.read()
         return filecontents
 
-    def upsert_to_socrata(client, dataset_identifier, zipper):
-        """
-        Upsert data to Socrata dataset.
-
-        :param client: Socrata connection client
-        :param dataset_identifier: Unique Socrata dataset identifier. Not the data page identifier but the primary page id.
-        :param zipper: dictionary of zipped results (headers and data values)
-        :return: None
-        """
-        try:
-            client.upsert(dataset_identifier=dataset_identifier, payload=zipper, content_type='json')
-        except Exception as e:
-            print("Error upserting to Socrata: {}. {}".format(dataset_identifier, e))
-        return
-
     # FUNCTIONALITY
     # Socrata related variables, derived
     credentials_json_file_contents = read_json_file(SOCRATA_CREDENTIALS_JSON_FILE)
@@ -128,6 +119,7 @@ def main():
     socrata_overview_level_dataset_app_id = get_dataset_identifier(credentials_json=credentials_json,
                                                                    dataset_key="overview_level_dataset")
 
+
     # __________________________________________________________________________
     # Overview level operations
     print("Entering Overview Dataset Operations")
@@ -137,23 +129,31 @@ def main():
 
     while more_overview_records_exist_than_response_limit_allows:
         overview_cycle_record_count = 0
+
+        # Only useful for understanding what the client.get call is doing
         overview_dataset_url = build_dataset_url(url_root=ROOT_URL_FOR_DATASET_ACCESS,
                                                  api_id=socrata_overview_level_dataset_app_id,
                                                  limit_amount=LIMIT_MAX_AND_OFFSET,
                                                  offset=overview_record_offset_value,
                                                  total_count=overview_total_record_count)
         print(overview_dataset_url)
-        overview_response = requests.get(url=overview_dataset_url)
-        overview_json = overview_response.json()
 
-        for obj in overview_json:
+        # for the private test datasets I had to use the client to access the data. May just move to this style.
+        overview_response = socrata_client_overview_level.get(dataset_identifier=socrata_overview_level_dataset_app_id,
+                                                              content_type="json",
+                                                              limit=LIMIT_MAX_AND_OFFSET,
+                                                              offset=overview_record_offset_value)
+
+        # for obj in overview_json:
+        for obj in overview_response:
             overview_date = obj.get("date", None)
             overview_date_obj = parser.parse(overview_date)
             overview_row_id = obj.get("row_id", None)
             if overview_date_obj < BASELINE_DATE:
-                overview_outdated_row_ids_list.append({":row_id": overview_row_id, ":deleted": True})
+                overview_outdated_row_ids_list.append({"row_id": overview_row_id, ":deleted": True})
 
-        number_of_overview_records_returned = len(overview_json)
+        # number_of_overview_records_returned = len(overview_json)
+        number_of_overview_records_returned = len(overview_response)
         overview_cycle_record_count += number_of_overview_records_returned
         overview_total_record_count += number_of_overview_records_returned
 
@@ -167,9 +167,12 @@ def main():
 
     print(overview_outdated_row_ids_list)
 
-    # socrata_client_overview_level.upsert(dataset_identifier=socrata_overview_level_dataset_app_id,
-    #                                      payload=overview_outdated_row_ids_list,
-    #                                      content_type='json')
+    delete_response_overview = socrata_client_overview_level.upsert(
+        dataset_identifier=socrata_overview_level_dataset_app_id,
+        payload=overview_outdated_row_ids_list,
+        content_type='json')
+    print(delete_response_overview)
+
     socrata_client_overview_level.close()
 
     # __________________________________________________________________________
@@ -180,39 +183,50 @@ def main():
     field_record_offset_value = 0
     while more_field_records_exist_than_response_limit_allows:
         field_cycle_record_count = 0
+
+        # Only useful for understanding what the client.get call is doing
         field_dataset_url = build_dataset_url(url_root=ROOT_URL_FOR_DATASET_ACCESS,
                                               api_id=socrata_field_level_dataset_app_id,
                                               limit_amount=LIMIT_MAX_AND_OFFSET,
                                               offset=field_record_offset_value,
                                               total_count=field_total_record_count)
         print(field_dataset_url)
-        field_response = requests.get(url=field_dataset_url)
-        field_json = field_response.json()
 
-        for obj in field_json:
+        field_response = socrata_client_field_level.get(dataset_identifier=socrata_field_level_dataset_app_id,
+                                                        content_type="json",
+                                                        limit=LIMIT_MAX_AND_OFFSET,
+                                                        offset=field_record_offset_value)
+
+        # for obj in field_json:
+        for obj in field_response:
             field_date = str(obj.get("date", None))
             field_date_obj = parser.parse(field_date)
             field_row_id = obj.get("row_id", None)
             if field_date_obj < BASELINE_DATE:
-                field_outdated_row_ids_list.append({":row_id": field_row_id, ":deleted": True})
+                field_outdated_row_ids_list.append({"row_id": field_row_id, ":deleted": True})
 
-        number_of_field_records_returned = len(field_json)
+        # number_of_field_records_returned = len(field_json)
+        number_of_field_records_returned = len(field_response)
         field_cycle_record_count += number_of_field_records_returned
         field_total_record_count += number_of_field_records_returned
 
         # Any cycle_record_count that equals the max limit indicates another request is needed
         if field_cycle_record_count == LIMIT_MAX_AND_OFFSET:
             # Give Socrata servers small interval before requesting more
+            print("sleeping")
             time.sleep(0.2)
             field_record_offset_value = field_cycle_record_count + field_record_offset_value
         else:
+            print("making it false")
             more_field_records_exist_than_response_limit_allows = False
 
     print(field_outdated_row_ids_list)
 
-    # socrata_client_overview_level.upsert(dataset_identifier=socrata_field_level_dataset_app_id,
-    #                                      payload=field_outdated_row_ids_list,
-    #                                      content_type='json')
+    delete_response_field = socrata_client_overview_level.upsert(
+        dataset_identifier=socrata_field_level_dataset_app_id,
+        payload=field_outdated_row_ids_list,
+        content_type='json')
+    print(delete_response_field)
 
     socrata_client_field_level.close()
 
